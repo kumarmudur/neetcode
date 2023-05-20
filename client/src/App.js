863e3d92bee9964f99a92323d305056f5a67b3d3
kumarmudur <kumarmudur10@gmail.com>
2023-05-20 22:26:38 +0530

    [ReactJS] app component changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter , Routes , Route } from "react-router-dom";
+
+import HomePage from "./Components/HomePage/HomePage"
+import AllProblems from "./Components/AllProblems/AllProblems";
+
+import Navbar from "./constants/navbar/Navbar"
+import ProblemsPage from "./components/problemsPage/ProblemsPage";
+import Signup from "./components/signup/Signup"
+import Login from "./components/login/Login"
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/problemset/all/" element={<AllProblems />} />
+                <Route path="/problems/:pid/" element={<ProblemsPage  />} />
+                <Route path="*" element={<div>404 Not Found</div>} />
+            </Routes>
+        </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
